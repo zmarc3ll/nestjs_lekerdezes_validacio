4c77c84b6249f2f8f44911ef21c0b61f5dc133b0
Zuber Marcell <zubermarcellpetrik@gmail.com>
2023-02-08 12:49:02 +0100

    initial comit

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,7 @@
-import { Body, Controller, Get, Post, Render } from '@nestjs/common';
-import { DataSource } from 'typeorm';
+import { Body, Controller, Get, NotFoundException, Param, Post, Query, Render } from '@nestjs/common';
+import { NotFoundError } from 'rxjs';
+import { DataSource, EntityNotFoundError } from 'typeorm';
+import Alkalmazott from './alkalmazott.entity';
 import { AppService } from './app.service';
 
 @Controller()
@@ -14,4 +16,28 @@ export class AppController {
   index() {
     return { message: 'Welcome to the homepage' };
   }
+
+  @Get('alkalmazott/search')
+  async searchAlkalmazott(@Query('email') email: string){
+    const alkalmazottRepo = this.dataSource.getRepository(Alkalmazott);
+    const [adat, darab] = await
+    alkalmazottRepo.createQueryBuilder().where('hivatalosEmail LIKE :email', {email: '%' + email+ '%' }).getManyAndCount();
+   return await alkalmazottRepo.findOneByOrFail({hivatalosEmail: email});
+  }
+
+  @Get('/alkalmazott/:id')
+  async getAlkalmazott(@Param('id') id: number) {
+    try{
+    const alkalmazottRepo = this.dataSource.getRepository(Alkalmazott);
+    return await alkalmazottRepo.findOneByOrFail({id: id});
+  } catch (e) {
+    if (e instanceof EntityNotFoundError) {
+      throw new NotFoundException('Az alkalmazott  nem létezik');
+    } else {
+      throw e;
+    }
+  }
+}
+
+
 }
